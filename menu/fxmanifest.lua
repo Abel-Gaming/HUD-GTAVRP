28d41ba94dd14ed970deafebdb1329e8fbbf7783
Tyler Abel <lcpdpolice4939@gmail.com>
2025-05-31 15:49:45 -0400

    Add files via upload

---
+++ menu/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- UI
+ui_page "ui/index.html"
+files {
+	"ui/index.html",
+	"ui/fonts/Circular-Bold.ttf",
+	"ui/fonts/Circular-Book.ttf",
+	"ui/assets/cursor.png",
+	"ui/assets/close.png",
+	"ui/front.js",
+	"ui/script.js",
+	"ui/style.css",
+	'ui/debounce.min.js'
+}
+
+-- Client Scripts
+client_scripts {
+		"client.lua",
+}
